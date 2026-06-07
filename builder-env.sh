6d876852140de776fcb29c7ece4c6549033cbb5c
Francesco Sodano <francesco-sodano@github.com>
2026-06-07 10:48:34 +0200

    First Release

---
+++ builder-env.sh
@@ -0,0 +1,5 @@
+# save as builder-env.sh
+export COPILOT_PROVIDER_BASE_URL=http://localhost:11434/v1
+export COPILOT_PROVIDER_API_KEY=          # blank for local Ollama
+export COPILOT_MODEL=devstral-small-2
+export COPILOT_OFFLINE=true               # fully air-gapped execution
